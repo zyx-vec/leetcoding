0511f672a528f20f4e3aa41230b0be59bbba6e77
deng <2942182050@qq.com>
2017-07-18 16:38:21 +0800

    Longest increasing subsequence.

--- src/LIS.cpp
+++ src/LIS.cpp
@@ -4,37 +4,36 @@
 
 class Solution {
 public:
-	int solve(const std::vector<int>& nums) {
-		if (nums.size() == 0)
-			return 0;
+    int solve(const std::vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
 
-		std::vector<int> table(nums.size(), 1);
+        int result = 1;
+        std::vector<int> table(nums.size(), 1);
 
-		for (int i = 1; i < nums.size(); i++) {
-			for (int j = 0; j < i; j++) {
-				if (nums[i] > nums[j]) {
-					table[i] = std::max(table[i], table[j]+1);
-				}
-			}
-		}
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    table[i] = std::max(table[i], table[j]+1);
+                    if (table[i] > result) {
+                        result = table[i];
+                    }
+                }
+            }
+        }
 
-		int result = table[0];
-		for (int i = 1; i < table.size(); i++) {
-			if (table[i] > result)
-				result = table[i];
-		}
-		return result;
-	}
+        return result;
+    }
 };
 
 int main() {
 
-	Solution sol;
+    Solution sol;
 
-	std::vector<int> nums = {1,3,6,7,9,4,10,5,6};
+    std::vector<int> nums = {1,3,6,7,9,4,10,5,6};
 
-	std::cout << sol.solve(nums) << std::endl;
+    std::cout << sol.solve(nums) << std::endl;
 
 
-	return 0;
+    return 0;
 }
